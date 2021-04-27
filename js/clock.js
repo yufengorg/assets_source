b81a1c16b63144ec0264f3a7a6c1c230f1e1f4fd
杨玉峰 <yangyufeng.web@qq.com>
2021-04-27 19:08:49 +0800

    修改样式

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,13 @@
 // 定义地址
 // var swfUrl = "http://chabudai.sakura.ne.jp/blogparts/honehoneclock/honehone_clock_tr.swf";
-var swfUrl = "https://cdn.jsdelivr.net/gh/yangyfeng/yangyfeng.github.io@gh-pages/assets_blog/media/honehone_clock_tr.swf";
+var swfUrl = "https://cdn.jsdelivr.net/gh/yufengorg/assets_source@main/media/honehone_clock_tr.swf";
 var swfTitle = "honehoneclock";
 
 function LoadBlogParts() {
 	var sUrl = swfUrl;
 	// codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0"
 	var sHtml = "";
-	sHtml += '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="https://cdn.jsdelivr.net/gh/yangyfeng/yangyfeng.github.io@gh-pages/assets_blog/media/swflash.cab#version=8,0,0,0" width="160" height="70" id="' + swfTitle + '" align="middle">';
+	sHtml += '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="https://cdn.jsdelivr.net/gh/yufengorg/assets_source@main/media/swflash.cab#version=8,0,0,0" width="160" height="70" id="' + swfTitle + '" align="middle">';
 	sHtml += '<param name="allowScriptAccess" value="always" />';
 	sHtml += '<param name="movie" value="' + sUrl + '" />';
 	sHtml += '<param name="quality" value="high" />';
